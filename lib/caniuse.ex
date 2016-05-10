7fe8b82497c786d9fed6be9997e3ba24dad3cc3c
Brian Volk <freshvolk@gmail.com>
2016-05-10 15:13:55 -0700

    Hello MVP

---
+++ lib/caniuse.ex
@@ -0,0 +1,30 @@
+defmodule Caniuse do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Start the endpoint when the application starts
+      supervisor(Caniuse.Endpoint, []),
+      # Start the Ecto repository
+      supervisor(Caniuse.Repo, []),
+      # Here you could define other workers and supervisors as children
+      # worker(Caniuse.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Caniuse.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    Caniuse.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
